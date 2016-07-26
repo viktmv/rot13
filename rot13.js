64de71706bd947ce632ecf2bacb528ad429df64a
Victor T <victor.tim@icloud.com>
2016-07-26 18:54:08 +0300

    formula refactoring

--- rot13.js
+++ rot13.js
@@ -12,7 +12,7 @@ function rot13(str) {
         return result.push(newChar(charCode))    
    
     else if (charCode <= 77 )
-        return result.push(newChar(90 - (12 - (charCode - 65) ) ) )
+        return result.push((newChar(charCode + 13)) 
     
     return result.push(newChar(charCode - 13) )
   })  
