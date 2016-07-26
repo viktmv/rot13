cb724d168d9486760d28c49de9ebdc2d2a5c11a9
Victor T <victor.tim@icloud.com>
2016-07-26 18:52:19 +0300

    spaces clean-up

--- rot13.js
+++ rot13.js
@@ -1,11 +1,11 @@
 // Caesar cipher for FreeCodeCamp
-
 'use strict'
 
 function rot13(str) { 
   let result = []
 
-  str.split('').forEach(function(v, i) {
+  str.split('').forEach(function(v, i) 
+  {
     let charCode = str.charCodeAt(i)
 
     if (charCode < 65)
@@ -16,13 +16,13 @@ function rot13(str) {
     
     return result.push(newChar(charCode - 13) )
   })  
-
   return result.join('');
 }
 
 function newChar(character) {
     return String.fromCharCode(character)
 }
+
 // Change the inputs below to test
 rot13("SERR PBQR PNZC");
 
