bb18b56fabe1503d1bee3eb584b28a3db657e87e
Victor T <victor.tim@icloud.com>
2016-07-26 19:03:21 +0300

    check frase change

--- rot13.js
+++ rot13.js
@@ -4,7 +4,7 @@
 function rot13(str) { 
   let result = []
 
-  str.split('').forEach(function(v, i) {
+  str.toUpperCase().split('').forEach(function(v, i) {
     let charCode = str.charCodeAt(i)
 
     if (charCode < 65)
@@ -23,5 +23,5 @@ function newChar(character) {
 }
 
 // Change the inputs below to test
-rot13("SERR PBQR PNZC");
+rot13("OYNPX FURRC JNYY");
 
