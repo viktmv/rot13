8db320e26a7db0d45a279679fd11cef00bc2c4cd
Victor T <victor.tim@icloud.com>
2016-07-26 18:49:01 +0300

    init

---
+++ rot13.js
@@ -0,0 +1,28 @@
+// Caesar cipher for FreeCodeCamp
+
+'use strict'
+
+function rot13(str) { 
+  let result = []
+
+  str.split('').forEach(function(v, i) {
+    let charCode = str.charCodeAt(i)
+
+    if (charCode < 65)
+        return result.push(newChar(charCode))    
+   
+    else if (charCode <= 77 )
+        return result.push(newChar(90 - (12 - (charCode - 65) ) ) )
+    
+    return result.push(newChar(charCode - 13) )
+  })  
+
+  return result.join('');
+}
+
+function newChar(character) {
+    return String.fromCharCode(character)
+}
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
+
