adf713ab91222837d3721f24f97a18e9d172fd46
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-07-26 19:31:22 +0300

    Update rot13.js

--- rot13.js
+++ rot13.js
@@ -2,8 +2,8 @@
 'use strict'
 
 function rot13(str) { 
-  return Array.from(str, char => {
-    let code = char.toUpperCase().charCodeAt()
+  return Array.from(str.toUpperCase(), char => {
+    let code = char.charCodeAt()
     if (code < 65) return char
 
     return String.fromCharCode(code > 77
